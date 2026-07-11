6ae3759f9f0f81557b913b0e6a0bb2b522160f29
Rahul Madeshiya <rahulmadeshiya851@gmail.com>
2026-07-11 13:37:18 +0530

    Hide footer on checkout page for enclosed checkout experience

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Lenis from 'lenis';
 import Navbar from '../components/layout/Navbar/Navbar';
 import Footer from '../components/layout/Footer/Footer';
@@ -12,13 +13,16 @@ export default function MainLayout({ children }) {
     return () => lenis.destroy();
   }, []);
 
+  const location = useLocation();
+  const isCheckout = location.pathname === '/checkout';
+
   return (
     <div className="app-wrapper">
       <Navbar />
       <main className="main-content">
         {children}
       </main>
-      <Footer />
+      {!isCheckout && <Footer />}
       <CartDrawer />
     </div>
   );
